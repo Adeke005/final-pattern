4adb3dd12ac8e9e483581eaa8ee124bf01cd1789
Adeke005 <Adeke1231230@gmail.com>
2026-05-06 04:03:10 +0500

    Tower System for new Design

---
+++ src/towers/ArrowTower.java
@@ -0,0 +1,9 @@
+package com.game.towerdefense.towers;
+
+import com.game.towerdefense.entities.Tower;
+
+public class ArrowTower extends Tower {
+    public ArrowTower(float x, float y) {
+        super(x, y, 15, 130f, 0.7f, 50);
+    }
+}
