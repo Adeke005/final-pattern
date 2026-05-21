22d364d6fdc8c2e3215f1befc16968f8d1d985ea
ubuntusila <kanurik2482540@gmail.com>
2026-05-21 05:41:47 +0500

    adding BossEnemy

---
+++ src/enemies/BossEnemy.java
@@ -0,0 +1,9 @@
+package com.game.towerdefense.enemies;
+
+import com.game.towerdefense.entities.Enemy;
+
+public class BossEnemy extends Enemy {
+    public BossEnemy(float x, float y) {
+        super(x, y, 300, 35f, 50, 150);
+    }
+}
